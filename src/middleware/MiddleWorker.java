e3695eabfc0160fc61257501330075108d8c6186
Hongyu Wu <wuhongyu@umich.edu>
2014-05-17 17:49:58 -0400

    record time

--- src/middleware/MiddleWorker.java
+++ src/middleware/MiddleWorker.java
@@ -9,6 +9,9 @@ public class MiddleWorker extends Thread {
 	private MiddleSocketChannel to;
 	private ByteBuffer buffer;
 
+	private long ts0, ts1, ts2, ts3, ts4;
+	private long t0 = 0, t1 = 0, t2 = 0, t3 = 0;
+
 	MiddleWorker(SharedData s) {
 		sharedData = s;
 		buffer = ByteBuffer.allocateDirect(sharedData.getMaxSize());
@@ -19,17 +22,35 @@ public class MiddleWorker extends Thread {
 	public void run() {
 
 		while (!sharedData.isEndOfProgram()) {
+			ts0 = System.currentTimeMillis();
+
 			SelectionKey key = sharedData.getSelectionKey();
 			from = sharedData.getSocket(key.channel());
 			to = (MiddleSocketChannel) key.attachment();
-			
-			if (to == null || from == null) continue;
+
+			ts1 = System.currentTimeMillis();
+
+			if (to == null || from == null)
+				continue;
 			buffer.clear();
 			int len = from.getInput(buffer);
+
+			ts2 = System.currentTimeMillis();
+
 			if (len <= 0)
 				continue;
 			to.sendOutput(buffer, len);
+
+			ts3 = System.currentTimeMillis();
+
+			t0 += ts1 - ts0;
+			t1 += ts2 - ts1;
+			t2 += ts3 - ts2;
 		}
+		long t = t0 + t1 + t2;
+		System.out.println("t0: " + ((double) t0)/t);
+		System.out.println("t1: " + ((double) t1)/t);
+		System.out.println("t2: " + ((double) t2)/t);
 
 	}
 
