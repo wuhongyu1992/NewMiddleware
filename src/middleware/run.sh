96f503d1caab49cc6595f15102ee26858500f9bc
Hongyu Wu <wuhongyu@umich.edu>
2014-05-17 23:23:27 -0400

    update run.sh

--- src/middleware/run.sh
+++ src/middleware/run.sh
@@ -1,3 +1,3 @@
-javac NewMiddleware.java MiddlewareUnit.java MiddleClient.java MiddleServer.java MiddleServerSocket.java MiddleSocketChannel.java SharedData.java MiddleWorker.java
+javac NewMiddleware.java MiddlewareUnit.java MiddleClient.java MiddleServer.java MiddleServerSocket.java MiddleSocketChannel.java SharedData.java MiddleWorker.java NewServerSocket.java NewWorker.java
 cd ..
 java middleware.NewMiddleware
